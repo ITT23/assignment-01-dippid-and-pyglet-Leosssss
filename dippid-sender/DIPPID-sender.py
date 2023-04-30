c337c8ed8983fcab3aecc5be910c9ec194208064
Yu <53040839+Leosssss@users.noreply.github.com>
2023-04-30 14:58:29 +0200

    a01.1 simulate random button_1

--- dippid-sender/DIPPID-sender.py
+++ dippid-sender/DIPPID-sender.py
@@ -1,6 +1,7 @@
 import socket
 import numpy as np
 import time
+import random
 
 IP = '127.0.0.1'
 PORT = 5700
@@ -19,10 +20,13 @@ while True:
     y = np.sin(sin_frequency * x)
     message = '{"coordinate(x,y)" : (' + str(x) + ',' + str(y) + ')}'
     print(message)
-
+    if counter%random.randint(7,10) == 0:
+        print('button1 released')
+        print('button1 pressed')
     sock.sendto(message.encode(), (IP, PORT))
 
     x += 0.1
+    counter += 1
     # change frequency
     sin_frequency += 0.01
     time.sleep(0.1)
